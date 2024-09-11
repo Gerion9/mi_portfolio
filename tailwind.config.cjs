1d539bef99a52d9698a53c310a9d3733359a0ea6
Gerion9 <gairo@berkeley.edu>
2024-09-11 16:52:27 -0600

    Commit inicial del proyecto de portafolio

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+      extend: {
+        colors: {
+          primary: '#4F46E5', // Indigo-600
+        },
+      },
+    },
+    plugins: [],
+  }
